436173ab8088eb1b12439bda3376baadf375c018
ëstevao97i <pedro.cordova2002@gmail.com>
2024-08-10 11:45:04 -0300

    fazendo request ao endpoint

--- pokedex/src/app/shared/poke-list/poke-list.component.ts
+++ pokedex/src/app/shared/poke-list/poke-list.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit{
 
-  
+  constructor(private service: PokeApiService) {}
 
+  ngOnInit(): void {
+    this.getAllPokemon();
+  }
+
+  getAllPokemon() {
+    this.service.getAllPokemon().subscribe({
+      next: (value: any) => console.log(value)
+    })
+  }
 }
