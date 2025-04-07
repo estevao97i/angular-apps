6c4e12ff87d5947db8acf833c6fa7718bbb2bf3b
ëstevao97i <pedro.cordova2002@gmail.com>
2025-04-07 20:49:21 -0300

    routes added with lazy loading

--- 1-roteamento-standalone/src/app/app.routes.ts
+++ 1-roteamento-standalone/src/app/app.routes.ts
@@ -4,9 +4,24 @@ import { SegundoComponent } from './components/segundo/segundo.component';
 import { BaseComponent } from './components/base/base.component';
 
 export const routes: Routes = [
-    { path: 'component', component: BaseComponent },
-    { path: 'component/primeiro', component: PrimeiroComponent, pathMatch: 'full' },
-    { path: 'component/segundo', component: SegundoComponent, pathMatch: 'full' },
+    // com lazy loading
+    { 
+        path: 'component', 
+        loadComponent: () => import('../app/components/base/base.component').then(m => m.BaseComponent)
+    },
+    { 
+        path: 'component/primeiro',
+        loadComponent: () => import('../app/components/primeiro/primeiro.component').then(m => m.PrimeiroComponent)
+    },
+    { 
+        path: 'component/segundo',
+        loadComponent: () => import('../app/components/segundo/segundo.component').then(m => m.SegundoComponent)
+    }
+
+    // { path: 'component', component: BaseComponent },
+    // { path: 'component/primeiro', component: PrimeiroComponent, pathMatch: 'full' },
+    // { path: 'component/segundo', component: SegundoComponent, pathMatch: 'full' },
+
     // { path: 'primeiro', component: PrimeiroComponent, pathMatch: 'full' },
     // { path: 'segundo', component: SegundoComponent, pathMatch: 'full' },
 ];
