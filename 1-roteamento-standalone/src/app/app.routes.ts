8e59f53720434c196ef5558cdcceb66255737aa4
ëstevao97i <pedro.cordova2002@gmail.com>
2025-04-06 23:23:38 -0300

    base route created

--- 1-roteamento-standalone/src/app/app.routes.ts
+++ 1-roteamento-standalone/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { PrimeiroComponent } from './components/primeiro/primeiro.component';
 import { SegundoComponent } from './components/segundo/segundo.component';
+import { BaseComponent } from './components/base/base.component';
 
 export const routes: Routes = [
-    { path: 'primeiro', component: PrimeiroComponent, pathMatch: 'full' },
-    { path: 'segundo', component: SegundoComponent, pathMatch: 'full' },
+    { path: 'component', component: BaseComponent },
+    { path: 'component/primeiro', component: PrimeiroComponent, pathMatch: 'full' },
+    { path: 'component/segundo', component: SegundoComponent, pathMatch: 'full' },
+    // { path: 'primeiro', component: PrimeiroComponent, pathMatch: 'full' },
+    // { path: 'segundo', component: SegundoComponent, pathMatch: 'full' },
 ];
