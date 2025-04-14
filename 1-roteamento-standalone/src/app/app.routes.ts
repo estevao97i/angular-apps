17dc8439d1ec632eb177c11d14200d6ac8104a95
ëstevao97i <pedro.cordova2002@gmail.com>
2025-04-13 23:46:02 -0300

    routerLinks added

--- 1-roteamento-standalone/src/app/app.routes.ts
+++ 1-roteamento-standalone/src/app/app.routes.ts
@@ -2,20 +2,50 @@ import { Routes } from '@angular/router';
 import { PrimeiroComponent } from './components/primeiro/primeiro.component';
 import { SegundoComponent } from './components/segundo/segundo.component';
 import { BaseComponent } from './components/base/base.component';
+import { InitalComponent } from './components/inital/inital.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PageAComponent } from './components/primeiro/components/page-a/page-a.component';
+import { PageBComponent } from './components/primeiro/components/page-b/page-b.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        title: 'Inicial',
+        component: InitalComponent,
+    },
     // com lazy loading
     { 
-        path: 'component', 
+        path: 'component',
+        title: 'Base',
         loadComponent: () => import('../app/components/base/base.component').then(m => m.BaseComponent)
     },
     { 
         path: 'component/primeiro',
-        loadComponent: () => import('../app/components/primeiro/primeiro.component').then(m => m.PrimeiroComponent)
+        title: 'Primeiro',
+        component: PrimeiroComponent,
+        children: [
+            {
+                path: 'page-a',
+                title: 'Page A',
+                component: PageAComponent
+            },
+            {
+                path: 'page-b',
+                title: 'Page B',
+                component: PageBComponent
+            }
+        ]
+        // loadComponent: () => import('../app/components/primeiro/primeiro.component').then(m => m.PrimeiroComponent)
     },
     { 
         path: 'component/segundo',
+        title: 'Segundo',
         loadComponent: () => import('../app/components/segundo/segundo.component').then(m => m.SegundoComponent)
+    },
+    {
+        path: '**',
+        title: 'Page not found',
+        component: NotFoundComponent,
     }
 
     // { path: 'component', component: BaseComponent },
---
+++ 1-roteamento-standalone/src/app/components/inital/inital.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inital',
+  standalone: true,
+  imports: [],
+  templateUrl: './inital.component.html',
+  styleUrl: './inital.component.scss'
+})
+export class InitalComponent {
+
+}
