b7d2c474c8755396ee40d9ed4bd330b30e4106eb
ëstevao97i <pedro.cordova2002@gmail.com>
2025-05-04 10:26:57 -0300

    adding post

---
+++ 1-roteamento-dinamico-input/src/app/services/post.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable, inject } from "@angular/core";
+import { Observable } from "rxjs";
+import { Post } from "../types/post.type";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PostService {
+
+    private readonly _http = inject(HttpClient);
+
+    getPostsByUser(userId: number): Observable<Post> {
+        return this._http.get<Post>(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+    }
+}
