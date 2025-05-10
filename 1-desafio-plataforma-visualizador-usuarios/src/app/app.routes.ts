aa37cb1322747cf9ff7d3d4a67237658b8a25d0f
ëstevao97i <pedro.cordova2002@gmail.com>
2025-05-10 09:47:05 -0300

    mapping components

--- 1-desafio-plataforma-visualizador-usuarios/src/app/app.routes.ts
+++ 1-desafio-plataforma-visualizador-usuarios/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { UserDetailComponent } from './components/user-detail/user-detail.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { AlbumComponent } from './components/album/album.component';
+import { PostComponent } from './components/post/post.component';
 
 export const routes: Routes = [
     {
@@ -16,7 +19,29 @@ export const routes: Routes = [
     {
         path: 'user-detail/:id',
         title: 'Detalhes do Usuário',
-        component: UserDetailComponent
+        component: UserDetailComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'todos',
+                pathMatch: 'full'
+            },
+            {
+                path: 'todos',
+                component: TodosComponent,
+                title: 'Todos',
+            },
+            {
+                path: 'album',
+                component: AlbumComponent,
+                title: 'Albuns',
+            },
+            {
+                path: 'post',
+                component: PostComponent,
+                title: 'Posts',
+            },
+        ]
     },
     {
         path: '**',
